5aebce63a222c00bd8cd827abe330e66ccc12787
Toni Barth <software@satoprogs.de>
2018-11-02 20:48:31 +0100

    initial commit * xamarin.forms project * already added all pages, but only create new game page yet connected

---
+++ stereopoly/stereopoly/stereopoly/CreateGamePage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace stereopoly
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class CreateGamePage : ContentPage
+	{
+		public CreateGamePage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
---
+++ stereopoly/stereopoly/stereopoly/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace stereopoly
+{
+  public partial class MainPage : ContentPage
+  {
+    public MainPage()
+    {
+      InitializeComponent();
+    }
+
+    async void OnCreate(object sender, EventArgs e)
+    {
+      await Navigation.PushAsync(new CreateGamePage());
+    }
+  }
+}
