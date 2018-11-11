5a42495263227ece5237dcee5e50110f0edbbd61
Toni Barth <software@satoprogs.de>
2018-11-11 23:33:54 +0100

    added error handling and an update button to create game page

--- stereopoly/stereopoly/stereopoly/CreateGamePage.xaml.cs
+++ stereopoly/stereopoly/stereopoly/CreateGamePage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -21,11 +22,38 @@ namespace stereopoly
 
     protected override async void OnAppearing()
     {
+      await this.UpdateBoards();
+    }
+
+    async void OnRefresh(object sender, EventArgs e)
+    {
+      this.BoardLayout.Children.Clear();
+      await this.UpdateBoards();
+    }
+
+    private async Task UpdateBoards()
+    {
       Button b;
       int i;
       List<Board> boards;
-      boards = await Caller.RequestBoards();
-      this.DownloadingIndicator.IsRunning=false;
+      this.DownloadingIndicator.IsRunning = true;
+      this.RefreshButton.IsEnabled = false;
+      try
+      {
+        boards = await Caller.RequestBoards();
+      }
+      catch (WebException ex)
+      {
+        boards = null;
+        await DisplayAlert("Error", "An error occurred while retrieving the list of known boards: " + ex.Message, "OK");
+      }
+      finally
+      {
+        this.DownloadingIndicator.IsRunning=false;
+        this.RefreshButton.IsEnabled = true;
+      }
+      if(boards == null)
+        return;
       for(i=0; i < boards.Count; i++)
       {
         b = new Button {
