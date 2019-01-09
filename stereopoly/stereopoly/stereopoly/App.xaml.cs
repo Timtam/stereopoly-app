cd4019ff5d61421aa3ea2dcaa2624bfe97d5fc15
Toni Barth <software@satoprogs.de>
2019-01-09 12:44:38 +0100

    added multi language support to app (only one string translated yet)

--- stereopoly/stereopoly/stereopoly/App.xaml.cs
+++ stereopoly/stereopoly/stereopoly/App.xaml.cs
@@ -1,32 +1,40 @@
 ﻿using System;
+using System.Globalization;
+using System.Resources;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 namespace stereopoly
 {
-    public partial class App : Application
+  public partial class App : Application
+  {
+    public App()
     {
-        public App()
-        {
-            InitializeComponent();
+      InitializeComponent();
 
-            MainPage = new NavigationPage(new MainPage());
-        }
-
-        protected override void OnStart()
-        {
-            // Handle when your app starts
-        }
-
-        protected override void OnSleep()
-        {
-            // Handle when your app sleeps
-        }
-
-        protected override void OnResume()
-        {
-            // Handle when your app resumes
-        }
+      MainPage = new NavigationPage(new MainPage());
     }
+
+    protected override void OnStart()
+    {
+      // Handle when your app starts
+      if (Device.RuntimePlatform == Device.iOS || Device.RuntimePlatform == Device.Android)
+      {
+        var ci = DependencyService.Get<ILocalize>().GetCurrentCultureInfo();
+        AppResources.Culture = ci; // set the RESX for resource localization
+        DependencyService.Get<ILocalize>().SetLocale(ci); // set the Thread for locale-aware methods
+      }
+    }
+
+    protected override void OnSleep()
+    {
+      // Handle when your app sleeps
+    }
+
+    protected override void OnResume()
+    {
+      // Handle when your app resumes
+    }
+  }
 }
---
+++ stereopoly/stereopoly/stereopoly/ILocalize.cs
@@ -0,0 +1,11 @@
+﻿using System.Globalization;
+using System.Resources;
+
+namespace stereopoly
+{
+  public interface ILocalize
+  {
+    CultureInfo GetCurrentCultureInfo();
+    void SetLocale(CultureInfo ci);
+  }
+}
